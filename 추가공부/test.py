fe823b78d377855d2efa2c93fa1738daaeaa287e
93TEI <93louis@gmail.com>
2020-01-02 16:48:01 +0900

    "hello, tensorflow"

--- 추가공부/test.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-print(tf.__version__)
-
-hello = tf.constant("Hello, Tensorflow") # 실제론 그래프에 위치하는 노드가 생성
-#그래프는 session.run으로 실행가능
-sess = tf.compat.v1.Session() #sess = tf.Session()
-
-print(sess.run(hello))
